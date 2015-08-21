dea68970e0b605fa27a2604f0fac5ae8e8bd2861
rosivanov <ros.g6w@gmail.com>
2015-08-21 20:17:58 +0300

    post

--- gulp/default.js
+++ gulp/default.js
@@ -3,7 +3,8 @@
 var gulp = require('gulp'),
 	config = require('./config'),
 	del = require('del');
-
+var postcss = require('gulp-postcss');
+var pixrem = require('pixrem');
 
 gulp.task('default', ['jade', 'sass', 'js', 'browser-sync', 'watch']);
 
@@ -11,3 +12,12 @@ gulp.task('default', ['jade', 'sass', 'js', 'browser-sync', 'watch']);
 gulp.task('gitkeep', function() {
 	return del([config.dev.gitkeep]);
 });
+
+
+gulp.task('post', function() {
+	var processors = [pixrem];
+	return gulp.src('dev/css/style.css')
+	.pipe(postcss(processors))
+	.pipe(gulp.dest('dev/css/new'));
+});
+
